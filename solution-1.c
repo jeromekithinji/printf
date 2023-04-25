8827c0976b672b76d16da1520f6383f1cb83946b
Herculesisdead <andrew@databit.net>
2023-04-25 13:07:35 -0700

    Solution-1.c

--- solution-1.c
+++ solution-1.c
@@ -1,62 +1,55 @@
-#include "main.h"
 #include <stdarg.h>
 #include <unistd.h>
 
 /**
- * _printf - produces output according to a format
- * @format: character string
- * Return: number of characters printed
+ * _printf - Prints formatted output.
+ * @format - Pointer to a str that contains a format str.
+ *
+ * Return - Number of characters printed
  */
-
 int _printf(const char *format, ...)
 {
-	va_list args;
-	int count = 0;
-	char *str;
-	char c;
+    va_list argument;
+    int print_ch = 0;
 
-	va_start(args, format);
-	while (*format)
-	{
-		if (*format == '%')
-		{
-			format++;
-			switch (*format)
-			{
-			case 'c':
-				c = va_arg(args, int);
-				write(1, &c, 1);
-				count++;
-				break;
-			case 's':
-				str = va_arg(args, char *);
-				if (!str)
-					str = "(null)";
-				while (*str)
-				{
-					write(1, str, 1);
-					str++;
-					count++;
-				}
-				break;
-			case '%':
-				write(1, "%", 1);
-				count++;
-				break;
-			default:
-				write(1, "%", 1);
-				write(1, &(*format), 1);
-				count += 2;
-				break;
-			}
-		}
-		else
-		{
-			write(1, &(*format), 1);
-			count++;
-		}
-		format++;
-	}
-	va_end(args);
-	return (count);
+    va_start(argument , format);
+
+    while (*format)
+    {
+        if (*format == '%')
+        {
+            format++;
+
+            switch (*format)
+            {
+                case 'c':
+                    print_ch += write(1, (char) va_arg(argument , int), 1);
+                    break;
+
+                case 's':
+                    print_ch += write(1, va_arg(argument , char *), 1);
+                    break;
+
+                case '%':
+                    print_ch += write(1, "%", 1);
+                    break;
+
+                default:
+                    print_ch += write(1, "%", 1);
+                    print_ch += write(1, format, 1);
+                    break;
+            }
+        }
+        else
+        {
+            print_ch += write(1, format, 1);
+        }
+
+        format++;
+    }
+
+    va_end(argument );
+
+    return (printed_chars);
 }
+
